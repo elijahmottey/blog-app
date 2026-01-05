1231fee9264241bbfecf6045cf1b6658fe875634
Elijah Mottey <elijahmottey5@gmail.com>
2026-01-05 12:41:30 +0000

    Initial commit

---
+++ src/main/java/liv/codveda/blog/app/repository/UsersRepository.java
@@ -0,0 +1,11 @@
+package liv.codveda.blog.app.repository;
+
+import liv.codveda.blog.app.domain.entities.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsersRepository extends JpaRepository<Users, Integer> {
+
+    Optional<Users> findByEmail(String email);
+}
