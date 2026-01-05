bbf0bff2ae9953c3aa300373179023ede6dcd651
Elijah Mottey <elijahmottey5@gmail.com>
2026-01-05 15:21:15 +0000

    blog entities with er diagram

---
+++ src/main/java/liv/codveda/blog/app/domain/entities/Comment.java
@@ -0,0 +1,49 @@
+package liv.codveda.blog.app.domain.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Comment content is required")
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private Users author;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    /**
+     * Timestamp automatically set when the user is created.
+     */
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    /**
+     * Timestamp automatically updated whenever the user record changes.
+     */
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+}
---
+++ src/main/java/liv/codveda/blog/app/domain/entities/Post.java
@@ -0,0 +1,51 @@
+package liv.codveda.blog.app.domain.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Title is required")
+    @Column(nullable = false, name = "title")
+    private String title;
+
+    @NotBlank(message = "Content is required")
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private Users users;
+
+    /**
+     * Timestamp automatically set when the user is created.
+     */
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    /**
+     * Timestamp automatically updated whenever the user record changes.
+     */
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+    private List<Comment> comments;
+
+}
