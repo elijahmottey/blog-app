24e7c13face4c891f927f8897529128515d7dd22
Elijah Mottey <elijahmottey5@gmail.com>
2026-01-06 16:41:50 +0000

    cors config

---
+++ src/main/java/liv/codveda/blog/app/security/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package liv.codveda.blog.app.security.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")   // IMPORTANT!
+                        .allowedOrigins(frontendUrl, "http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
