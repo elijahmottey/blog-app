db9295025b2de640c008fdd5f9a465f0306aa93a
Elijah Mottey <elijahmottey5@gmail.com>
2026-01-08 19:54:42 +0000

    user service

--- src/main/java/liv/codveda/blog/app/repository/UsersRepository.java
+++ src/main/java/liv/codveda/blog/app/repository/UsersRepository.java
@@ -2,9 +2,12 @@ package liv.codveda.blog.app.repository;
 
 import liv.codveda.blog.app.domain.entities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+
+@Repository
 public interface UsersRepository extends JpaRepository<Users, Integer> {
 
     Optional<Users> findByEmail(String email);
