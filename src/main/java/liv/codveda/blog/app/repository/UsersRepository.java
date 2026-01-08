199a192940986222c3dd5766798b130a27474b1b
Elijah Mottey <elijahmottey5@gmail.com>
2026-01-08 20:29:35 +0000

    update users and user history and info

--- src/main/java/liv/codveda/blog/app/repository/UsersRepository.java
+++ src/main/java/liv/codveda/blog/app/repository/UsersRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 
 @Repository
-public interface UsersRepository extends JpaRepository<Users, Integer> {
+public interface UsersRepository extends JpaRepository<Users, Long> {
 
     Optional<Users> findByEmail(String email);
     boolean existsByEmail(String email);
