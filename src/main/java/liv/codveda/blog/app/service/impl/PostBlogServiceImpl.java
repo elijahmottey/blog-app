5d5be457e17298430d573d8bc5a99ffba98acee3
Elijah Mottey <elijahmottey5@gmail.com>
2026-01-09 13:13:08 +0000

    post blog service class

--- src/main/java/liv/codveda/blog/app/service/impl/PostBlogServiceImpl.java
+++ src/main/java/liv/codveda/blog/app/service/impl/PostBlogServiceImpl.java
@@ -1,38 +1,61 @@
 package liv.codveda.blog.app.service.impl;
 
+import jakarta.persistence.EntityNotFoundException;
 import liv.codveda.blog.app.domain.entities.Post;
+import liv.codveda.blog.app.repository.PostRepository;
 import liv.codveda.blog.app.service.interfaces.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
+@Service
 public class PostBlogServiceImpl implements BlogService {
+    private final PostRepository postRepository;
+    @Autowired
+    public PostBlogServiceImpl(PostRepository postRepository, BlogService blogService) {
+        this.postRepository = postRepository;
+    }
+
     @Override
     public Post postBlog(Post post) {
-        return null;
+        return postRepository.save(post);
     }
 
     @Override
     public void deletePost(Long id) {
+        this.getPostById(id);
+        this.postRepository.deleteById(id);
+
 
     }
 
     @Override
     public List<Post> getPosts() {
-        return List.of();
+        return this.postRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
     }
 
     @Override
     public Post getPostById(Long id) {
-        return null;
+        return this.postRepository.findById(id)
+                .orElseThrow(
+                        ()-> new EntityNotFoundException("user with "+id +" not found"));
     }
 
     @Override
     public Post getPostByTitle(String title) {
-        return null;
+        return postRepository.getPostByTitle(title);
     }
 
     @Override
     public Post updatePost(Long id, Post post) {
-        return null;
+        if(!Objects.equals(post.getId(), id))
+            throw new EntityNotFoundException("user with "+id +" not found");
+        Post existingPost = this.getPostById(id);
+        if(post.getTitle() != null) existingPost.setTitle(post.getTitle());
+        if (post.getContent() != null) existingPost.setContent(post.getContent());
+        return postRepository.save(existingPost);
     }
 }
