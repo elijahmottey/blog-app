56bf519e0e204e90dd37deb53f9270ca565741b9
Elijah Mottey <elijahmottey5@gmail.com>
2026-01-14 12:22:13 +0000

    use api hook

---
+++ frontend/blog-app/src/hooks/useApi.tsx
@@ -0,0 +1,41 @@
+import { useCallback, useState } from "react";
+import type { ApiError } from "../service/BackendApi.ts";
+
+type ApiFunction<T> = () => Promise<T>;
+
+export function useApi<T>(apiFn: ApiFunction<T>, immediate = false) {
+    const [data, setData] = useState<T | null>(null);
+    const [error, setError] = useState<ApiError | null>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const execute = useCallback(async () => {
+        setLoading(true);
+        setError(null);
+
+        try {
+            const response = await apiFn();
+            setData(response);
+            return response;
+        } catch (err) {
+            setError(err as ApiError);
+            throw err;
+        } finally {
+            setLoading(false);
+        }
+    }, [apiFn]);
+
+    // Optional auto-run
+    useState(() => {
+        if (immediate) {
+            execute();
+        }
+    });
+
+    return {
+        data,
+        error,
+        loading,
+        execute,
+        setData,
+    };
+}
